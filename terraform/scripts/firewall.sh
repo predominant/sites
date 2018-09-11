20fa1e854d0ad6c3fabfeb85aab20c81634f3716
Graham Weldon <graham@grahamweldon.com>
2018-09-11 13:25:25 +0900

    Added firewall
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

---
+++ terraform/scripts/firewall.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND
+
+sudo apt-get install -y ufw
+
+# General rules
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+# Specific rules
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+
+# Engage
+sudo ufw enable
+
+# Display rule sets
+sudo ufw status
