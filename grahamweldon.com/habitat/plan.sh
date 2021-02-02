f374021ef660369f384e1c87804b827e853b021e
Graham Weldon <graham@grahamweldon.com>
2021-02-02 10:22:57 +0900

    Version bump
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- grahamweldon.com/habitat/plan.sh
+++ grahamweldon.com/habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=site-grahamweldon
 pkg_origin=grahamweldon
-pkg_version="2020.01.07.0"
+pkg_version=2021.02.02.0
 pkg_maintainer="Graham Weldon <graham@grahamweldon.com>"
 pkg_license=("Apache-2.0")
 pkg_description="Personal website of Graham Weldon"
