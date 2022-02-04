dad79e8621323b339000dd47224435b3f7f2b62c
Graham Weldon <graham@grahamweldon.com>
2022-02-04 14:45:14 +0900

    Update to Caddy 2 (own origin)
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- loadbalancer/habitat/plan.sh
+++ loadbalancer/habitat/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=site-loadbalancer
 pkg_origin=grahamweldon
-pkg_version="2022.02.04.0"
+pkg_version="2022.02.04.3"
 pkg_maintainer="Graham Weldon <graham@grahamweldon.com>"
 pkg_license=("Apache-2.0")
 pkg_description="Front load balancer for all my sites"
 pkg_deps=(
-  core/caddy
+  grahamweldon/caddy
 )
-pkg_svc_run="caddy -conf ${pkg_svc_config_path}/Caddyfile -log ${svc_var_path}/caddy.log"
+pkg_svc_run="caddy run -config ${pkg_svc_config_path}/Caddyfile"
 pkg_exports=(
   [http-port]=http.port
   [https-port]=https.port
