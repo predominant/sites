2edc8a5b5c7ea25e7509a5a1bf0bda0a5c18b65c
Graham Weldon <graham@grahamweldon.com>
2022-08-16 20:29:15 +0900

    Update plan.sh

--- grahamweldon.com/habitat/plan.sh
+++ grahamweldon.com/habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=site-grahamweldon
 pkg_origin=grahamweldon
-pkg_version=2021.02.02.0
+pkg_version=2022.08.16.0
 pkg_maintainer="Graham Weldon <graham@grahamweldon.com>"
 pkg_license=("Apache-2.0")
 pkg_description="Personal website of Graham Weldon"
